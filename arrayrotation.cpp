e805cb7bc25f0c17db501806b8ee6fb9046cc449
Niranth Sai <sirusalansai.ece18@itbhu.ac.in>
2021-10-29 20:47:20 +0530

    Update arrayrotation.cpp

--- arrayrotation.cpp
+++ arrayrotation.cpp
@@ -3,13 +3,7 @@ using namespace std;
  
 /*Function to left Rotate arr[] of
   size n by 1*/
-void leftRotatebyOne(int arr[], int n)
-{
-    int temp = arr[0], i;
-    for (i = 0; i < n - 1; i++)
-        arr[i] = arr[i + 1];
- 
-    arr[n-1] = temp;
+void l
 }
  
 /*Function to left rotate arr[] of size n by d*/
